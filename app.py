1e436b5a2d4b08cec350f2340457e868e69e090f
md-naim-molla <naim.molla.stats@gmail.com>
2025-12-09 12:14:23 +0600

    'Initial commit'

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+# VADER sentiment Score
+from nltk.sentiment import SentimentIntensityAnalyzer
+sia = SentimentIntensityAnalyzer()
+
+st.title('Sentiment Analysis Dashboard')
+st.subheader('This Dashboard will provide you a sentiment score for a partucular text.')
+
+
+
+text = st.text_input('Enter your text: ')
+btn = st.button('Get Sentiment')
+
+
+def get_sentiment(text):
+    scores = sia.polarity_scores(text)
+    return scores
+
+if btn:
+     scores = get_sentiment(text)
+
+     if scores['compound'] >= 0.05:
+        st.write('Positive Sentiment')
+     elif scores['compound'] <= -0.05:
+        st.write('Negative Sentiment')
+     else:
+        st.write('Neutral Sentiment')
+     
+    
+       
+
+
+
+
+
+
