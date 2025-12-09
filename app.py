b5c5811183f6f295d8fcb857bfd366b16813f8f0
md-naim-molla <naim.molla.stats@gmail.com>
2025-12-09 12:22:46 +0600

    'some correction'

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 # VADER sentiment Score
 from nltk.sentiment import SentimentIntensityAnalyzer
 sia = SentimentIntensityAnalyzer()
+import nltk
+nltk.download('vader_lexicon')
 
 st.title('Sentiment Analysis Dashboard')
 st.subheader('This Dashboard will provide you a sentiment score for a partucular text.')
